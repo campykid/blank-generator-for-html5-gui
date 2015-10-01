cb64a82f80e80e2c58ac1a54fd6364c221b2811d
Владислав Хвостов <vladkhvo@gmail.com>
2015-10-01 09:36:13 +0300

    ewe

---
+++ app/template.js
@@ -0,0 +1,20 @@
+// Болванка.
+var skelet = [ 
+'<!DOCTYPE html>',
+'\n<html>',
+'\n<head>\n\t<meta charset="UTF-8">',
+'\n\t<title>Document</title>',
+'\n\t<style>',
+'\n',
+'\n\t</style>',
+'\n</head>',
+'\n\t<body>',
+'\n',	
+'\n\t</body>',
+'\n\t<script type="text/javascript">',
+'\n',
+'\n\t</script>',
+'\n</html>'
+].join('')
+
+module.exports.template = skelet;
