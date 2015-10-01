d0e1b38cd20637c1679fb13985dd71f6e63ebf63
Владислав Хвостов <vladkhvo@gmail.com>
2015-10-01 09:37:38 +0300

    add modules

--- app/node-logic.js
+++ app/node-logic.js
@@ -1,23 +1,4 @@
-// Болванка.
-var skelet = [ 
-'<!DOCTYPE html>',
-'\n<html>',
-'\n<head>\n\t<meta charset="UTF-8">',
-'\n\t<title>Document</title>',
-'\n\t<style>',
-'\n',
-'\n\t</style>',
-'\n</head>',
-'\n\t<body>',
-'\n',	
-'\n\t</body>',
-'\n\t<script type="text/javascript">',
-'\n',
-'\n\t</script>',
-'\n</html>'
-].join('')
-
-
+var template = require('template');
 var fs = require('fs');
 
 
@@ -34,7 +15,7 @@ var fs = require('fs');
 
 // Созадает файл с болванкой.
 function createFile() {
-	fs.writeFile(path + "index.html", skelet, function(err) {
+	fs.writeFile(path + "index.html", template, function(err) {
 	    if(err) alert ('Ошибка - ' + err);
 	    console.log("The file was created!");
 	    var close = window.confirm('Файл успешно создан и лежит в home/username/Шаблоны. Вы желаете выйти?');
